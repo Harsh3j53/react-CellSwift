bf312f4656bedc469de6e198479ebbeaba885cb0
Harsh3j53 <harsh3j53@gmail.com>
2023-11-04 18:44:19 +0530

    NEW

--- client/src/components/Home/Category/Category.jsx
+++ client/src/components/Home/Category/Category.jsx
@@ -5,6 +5,9 @@ const Category = ({ categories }) => {
   const navigate = useNavigate();
   return (
     <div className="shop-by-category">
+      <div className="section-heading">
+        Categories:
+      </div>
       <div className="categories">
         {categories?.data?.map((item) => (
           <div key={item.id} className="category" onClick={()=> navigate(`/category/${item.id}`)}>
