2430e82f798d9cdcb1bbc017a492c75ce24454f7
Harsh3j53 <harsh3j53@gmail.com>
2023-11-05 23:30:57 +0530

    last

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,15 +1,29 @@
 import "./Products.scss";
 import Product from "./Product/Product";
+
 const Products = ({ products, innerPage, headingText }) => {
+  // Check if products is undefined or empty
+  if (!products || !products.data || products.data.length === 0) {
+    return (
+      <div className="products-container">
+        {!innerPage && <div className="sec-heading">{headingText}</div>}
+        <div className="products">No products available</div>
+      </div>
+    );
+  }
+
+  // Limit the products to the first 8 items
+  const limitedProducts = products.data.slice(0, 8);
+
   return (
     <div className="products-container">
       {!innerPage && <div className="sec-heading">{headingText}</div>}
       <div className="products">
-        {products?.data?.map((item) => (
+        {limitedProducts.map((item) => (
           <Product 
-          key={item.id} 
-          id={item.id} 
-          data={item.attributes} 
+            key={item.id} 
+            id={item.id} 
+            data={item.attributes} 
           />
         ))}
       </div>
@@ -18,3 +32,4 @@ const Products = ({ products, innerPage, headingText }) => {
 };
 
 export default Products;
+
